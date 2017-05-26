dddd52b7e131c3419f36a68ccb2d28cfe5aa2448
Justin Lee <justin0022@hotmail.com>
2017-05-25 21:21:31 -0700

    Added back sampleD3 for now

--- projects/course-pathway-project/client/controller.js
+++ projects/course-pathway-project/client/controller.js
@@ -1,8 +1,12 @@
 import initEventListeners from './eventListeners'
 import { getCourses } from './util'
+import sampleD3 from './sampleD3Viz'
 
 const controller = (data, course = "ARTS210") => {
     initEventListeners(getCourses(data))
+    
+    // just a sample. will remove later
+    sampleD3()
 }
 
 export default controller
