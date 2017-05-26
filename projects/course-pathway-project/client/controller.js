ce9e4e3ed139d3b68a76a3aa0892a32a61cce7cf
Justin Lee <justin0022@hotmail.com>
2017-05-25 21:01:57 -0700

    Implemented sorting of courses

--- projects/course-pathway-project/client/controller.js
+++ projects/course-pathway-project/client/controller.js
@@ -3,7 +3,6 @@ import { getCourses } from './util'
 
 const controller = (data, course = "ARTS210") => {
     initEventListeners(getCourses(data))
-    
 }
 
 export default controller
--- projects/course-pathway-project/client/util.js
+++ projects/course-pathway-project/client/util.js
@@ -1,5 +1,18 @@
 import R from 'ramda'
 
+// horribly ugly way to sort courses :(
+const sortCourses = (courses) => R.sort((a, b) => {
+    let first = Number(a.slice(-3))
+    let second = Number(b.slice(-3))
+    if (R.test(/^[a-zA-Z]*$/, a.slice(-1))) {
+        first = Number(a.slice(-4, -1))
+    }
+    if (R.test(/^[a-zA-Z]*$/, b.slice(-1))) {
+        second = Number(b.slice(-4, -1))
+    }
+    return first - second
+}, courses)
+
 const getCourses = (data) => {
     const courses = []
     R.map(student =>
@@ -7,7 +20,7 @@ const getCourses = (data) => {
             courses.push(x.courseSubject + x.courseNumber)
         , student)
     , data.students)
-    return R.uniq(courses)
+    return sortCourses(R.uniq(courses))
 }
 
 export {
