58623bea6d39f1d6b5d592fdeab31ead207cd7d4
Justin Lee <justin.lee@ubc.ca>
2017-06-02 16:26:27 -0700

    removing all child from old graphs before drawing

--- projects/course-pathway-project/client/controller.js
+++ projects/course-pathway-project/client/controller.js
@@ -4,20 +4,12 @@ import R from 'ramda'
 
 const controller = (data, courseName = getCourseNames(data)[0]) => {
     
-
-
-    // just for testing purposes. this is a popular course
-    //courseName = 'ARTS268'
-
-    
-
-    // returns  { before: [], current: [], after: [], courseName: ''}
     const dataToViz = R.pipe(
         getBeforeCurrentAndAfter,
         convertToNodes
-
     ) 
 
+    // initial draw
     draw(dataToViz(data, courseName))
 }
 
--- projects/course-pathway-project/client/setup.js
+++ projects/course-pathway-project/client/setup.js
@@ -1,4 +1,5 @@
 import controller from './controller'
+import * as d3 from 'd3'
 
 const initSelector = (courses) => {
     document.getElementById('course-list').innerHTML = courses.map(x => 
@@ -8,6 +9,8 @@ const initSelector = (courses) => {
 
 const initEventListener = (data) => {
     document.getElementById('course-list').addEventListener('change', function() {
+
+        d3.selectAll('svg').remove()
         // redraw on selection
         controller(data, this.value)
     })
