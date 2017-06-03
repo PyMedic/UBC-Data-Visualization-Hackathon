1511f623f98140e9acd331ca735b755aca617ee4
Justin Lee <justin.lee@ubc.ca>
2017-06-02 17:33:03 -0700

    Last commit, happy Friday

--- projects/course-pathway-project/client/draw.js
+++ projects/course-pathway-project/client/draw.js
@@ -13,6 +13,9 @@ const height = 700 - margin.top - margin.bottom
 const draw = (data) => {
     console.log(data)
     const nodes = data.nodes
+    const curCourse = {
+        courseName: data.courseName
+    }
 
     const svg = d3.select('#graph')
         .append('svg')
@@ -22,7 +25,7 @@ const draw = (data) => {
         .attr('viewBox', '0 0 ' + Math.min(graphWidth, height) + ' ' + 700)
         .attr('preserveAspectRatio', 'xMinYMin')
 
-    const g = svg.append('g').attr("transform", "translate(" + margin.left + "," + margin.top + ")")    
+    const g = svg.append('g').attr("transform", "translate(" + margin.left + "," + (margin.top + 200) + ")")    
 
     const tick = () => {
         circle
@@ -30,7 +33,11 @@ const draw = (data) => {
             .attr("cy", function(d) { return d.y; });
     }
 
-    const forceCollide = d3.forceCollide()
+    const forceCollideBefore = d3.forceCollide()
+        .radius((d) => d.numberOfCourses + 10)
+        .iterations(1)
+
+    const forceCollideAfter = d3.forceCollide()
         .radius((d) => d.numberOfCourses + 10)
         .iterations(1)
 
@@ -38,29 +45,39 @@ const draw = (data) => {
         .nodes(nodes.filter(d => d.time === 'before'))
         .force('X', d3.forceX(20).strength(1))
         .force('Y', (d) => { 
-            return d3.forceY(height).strength(1)
+            return d3.forceY(-100).strength(1)
         })
         //.force('center', d3.forceCenter(graphWidth /2, height / 2))
-        .force("charge", d3.forceManyBody())
-        .force("collide", forceCollide)
+        .force("charge", d3.forceManyBody().strength(0.3))
+        .force("collide", forceCollideBefore)
         .on("tick", tick)
     
     const forceAfter = d3.forceSimulation()
         .nodes(nodes.filter(d => d.time === 'after'))
         .force('X', d3.forceX(graphWidth).strength(1))
         .force('Y', (d) => { 
-            return d3.forceY(height).strength(1)
+            return d3.forceY(-100).strength(1)
         })
         //.force('center', d3.forceCenter(graphWidth /2, height / 2))
-        .force("charge", d3.forceManyBody().strength(1))
-        .force("collide", forceCollide)
+        .force("charge", d3.forceManyBody().strength(0.3))
+        .force("collide", forceCollideAfter)
+        .on("tick", tick)
+
+    const forceCurrent = d3.forceSimulation()
+        .nodes([curCourse])
+        .force('center', d3.forceCenter(graphWidth /2, height / 2))
         .on("tick", tick)
 
     const circle = g.selectAll('circle')
         .data(nodes)
         .enter().append('circle')
         .attr("r", (d) => Math.pow(Math.log(d.numberOfCourses*8), 2))
-        
+    
+    // const curCourseCircle = g.selectAll('circle')
+    //     .data([curCourse])
+    //     .enter().append('circle')
+    //     .attr('r', 10)
+    
 }
 
 export default draw
